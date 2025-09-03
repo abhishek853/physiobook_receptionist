feecca45c9ea0601a498a6056c9107c8637982b0
Abhishek sukhadev wankhade <119746259+abhishek853@users.noreply.github.com>
2025-09-03 18:29:02 +0530

    Add files via upload

---
+++ receptionist/backend/physiobook_receptionist/src/main/java/com/physiobook/physiobook_receptionist/PhysiobookReceptionistApplication.java
@@ -0,0 +1,13 @@
+package com.physiobook.physiobook_receptionist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysiobookReceptionistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysiobookReceptionistApplication.class, args);
+	}
+
+}
---
+++ receptionist/backend/physiobook_receptionist/src/test/java/com/physiobook/physiobook_receptionist/PhysiobookReceptionistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.physiobook.physiobook_receptionist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysiobookReceptionistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
